5b457880b8d913b9c1ac37f89f9cfed0b17d6884
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2024-03-09 14:11:25 -0600

    Anti Aliasing Agregado

--- src/Canvas.java
+++ src/Canvas.java
@@ -25,7 +25,8 @@ public class Canvas extends JFrame {
     public void paint(Graphics g) {
         super.paint(g);
         lineaBresenham(100, 100, 500, 500, Color.red);
-        antiAliasedLine(100, 500, 500, 300, Color.blue);
+        antiAliasedLine(500, 100, 100, 300, Color.blue);
+        antiAliasedLine(500, 100, 100, 500, Color.blue);
         getGraphics().drawImage(buffer,0,0,panel);
     }
     public void antiAliasedLine(double x1, double y1, double x2, double y2, Color a) {
@@ -34,32 +35,68 @@ public class Canvas extends JFrame {
         double dy = y2 - y1;
         double m = dy / dx;
         double b = y1 - (m * x1);
-        if(dy >= dx) {
-            for(int i = (int) floor(y1); i <= (int) floor(y2); i++) {
-                int tempX = (int) floor((i - b) / m);
-                pixel(tempX, i, Color.blue, 255);
+        if(abs(dy) == abs(dx)) {
+            if(x1 > x2) {
+                double tmp = x1;
+                x1 = x2;
+                x2 = tmp;
             }
-            for(int i = (int) floor(y1); i >= (int) floor(y2); i--) {
-                int tempX = (int) floor((i - b) / m);
-                pixel(tempX, i, Color.blue, 255);
+            for(int i = (int) floor(x1); i <= (int) floor(x2); i++) {
+                double midX = i + 0.5;
+                double midY = (m * midX) + b;
+                double tempY = floor(midY);
+                double q = midY - tempY;
+                pixel(i, (int) tempY, a, 255);
+                pixel(i, (int) tempY + 1, a, (int) ((0.25) * 255));
+                pixel(i, (int) tempY - 1, a, (int) ((0.25) * 255));
             }
         }
         else {
-            for(int i = (int) floor(x1); i <= (int) floor(x2); i++) {
-                //pixel(i, tempY, Color.blue, 255);
-                double midX = i + 0.5;
-                double midY = (m * midX) + b;
-                double q = midY - floor(midY);
-                if(q > 0.5) {
-                    double percent = abs(0.5 - q) * 2;
+            if(abs(dy) >= abs(dx)) {
+                if(y1 > y2) {
+                    double tmp = y1;
+                    y1 = y2;
+                    y2 = tmp;
                 }
-                else {
-
+                for(int i = (int) floor(y1); i <= (int) floor(y2); i++) {
+                    double midY = i + 0.5;
+                    double midX = (midY - b) / m;
+                    double tempX = floor(midX);
+                    double q = midX - tempX;
+                    if(q > 0.5) {
+                        double percent = 1 - (q - 0.5);
+                        pixel((int) tempX, i, a, (int) (percent * 255));
+                        pixel((int) tempX + 1, i, a, (int) ((1 - percent) * 255));
+                    }
+                    else {
+                        double percent = 1 - (0.5 - q);
+                        pixel((int) tempX, i, a, (int) (percent * 255));
+                        pixel((int) tempX - 1, i, a, (int) ((1 - percent) * 255));
+                    }
                 }
             }
-            for(int i = (int) floor(x1); i >= (int) floor(x2); i--) {
-                int tempY = (int) floor((m * i) + b);
-                pixel(i, tempY, Color.blue, 255);
+            else {
+                if(x1 > x2) {
+                    double tmp = x1;
+                    x1 = x2;
+                    x2 = tmp;
+                }
+                for(int i = (int) floor(x1); i <= (int) floor(x2); i++) {
+                    double midX = i + 0.5;
+                    double midY = (m * midX) + b;
+                    double tempY = floor(midY);
+                    double q = midY - tempY;
+                    if(q > 0.5) {
+                        double percent = 1 - (q - 0.5);
+                        pixel(i, (int) tempY, a, (int) (percent * 255));
+                        pixel(i, (int) tempY + 1, a, (int) ((1 - percent) * 255));
+                    }
+                    else {
+                        double percent = 1 - (0.5 - q);
+                        pixel(i, (int) tempY, a, (int) (percent * 255));
+                        pixel(i, (int) tempY - 1, a, (int) ((1 - percent) * 255));
+                    }
+                }
             }
         }
         long finish = System.nanoTime();
@@ -149,82 +186,6 @@ public class Canvas extends JFrame {
                 pixel((int) x, y, a, 255);
                 x += (1/m);
             }
-        }
-    }
-
-    int ipart(double x) {
-        return (int) x;
-    }
-
-    double fpart(double x) {
-        return x - floor(x);
-    }
-
-    double rfpart(double x) {
-        return 1.0 - fpart(x);
-    }
-
-    void plot(Graphics2D g, double x, double y, double c) {
-        g.setColor(new Color(0f, 0f, 0f, (float)c));
-        g.fillOval((int) x, (int) y, 2, 2);
-    }
-
-    void drawLine(Graphics2D g, double x0, double y0, double x1, double y1) {
-
-        boolean steep = abs(y1 - y0) > abs(x1 - x0);
-        if (steep)
-            drawLine(g, y0, x0, y1, x1);
-
-        if (x0 > x1)
-            drawLine(g, x1, y1, x0, y0);
-
-        double dx = x1 - x0;
-        double dy = y1 - y0;
-        double gradient = dy / dx;
-
-        // handle first endpoint
-        double xend = round(x0);
-        double yend = y0 + gradient * (xend - x0);
-        double xgap = rfpart(x0 + 0.5);
-        double xpxl1 = xend; // this will be used in the main loop
-        double ypxl1 = ipart(yend);
-
-        if (steep) {
-            plot(g, ypxl1, xpxl1, rfpart(yend) * xgap);
-            plot(g, ypxl1 + 1, xpxl1, fpart(yend) * xgap);
-        } else {
-            plot(g, xpxl1, ypxl1, rfpart(yend) * xgap);
-            plot(g, xpxl1, ypxl1 + 1, fpart(yend) * xgap);
-        }
-
-        // first y-intersection for the main loop
-        double intery = yend + gradient;
-
-        // handle second endpoint
-        xend = round(x1);
-        yend = y1 + gradient * (xend - x1);
-        xgap = fpart(x1 + 0.5);
-        double xpxl2 = xend; // this will be used in the main loop
-        double ypxl2 = ipart(yend);
-
-        if (steep) {
-            plot(g, ypxl2, xpxl2, rfpart(yend) * xgap);
-            plot(g, ypxl2 + 1, xpxl2, fpart(yend) * xgap);
-        } else {
-            plot(g, xpxl2, ypxl2, rfpart(yend) * xgap);
-            plot(g, xpxl2, ypxl2 + 1, fpart(yend) * xgap);
-        }
-
-        // main loop
-        for (double x = xpxl1 + 1; x <= xpxl2 - 1; x++) {
-            if (steep) {
-                plot(g, ipart(intery), x, rfpart(intery));
-                plot(g, ipart(intery) + 1, x, fpart(intery));
-            } else {
-                plot(g, x, ipart(intery), rfpart(intery));
-                plot(g, x, ipart(intery) + 1, fpart(intery));
-            }
-            intery = intery + gradient;
         }
     }
 
